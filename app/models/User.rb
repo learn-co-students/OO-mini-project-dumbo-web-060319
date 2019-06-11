eb35773296f5b03693f211926887c460383e6301
Alexander Schelchere <ascellie@gmail.com>
2019-06-11 09:24:31 -0400

    New top three recipes, now works!

--- app/models/User.rb
+++ app/models/User.rb
@@ -43,7 +43,27 @@ class User
 
     def top_three_recipes 
         # should return the top three highest rated recipes for this user.
-        self.my_recipe_cards.select { |recipe_card| recipe_card.recipe if recipe_card.rating.max(3) }
+        # array to hold the top three
+        top_three = []
+        # placeholder for first second and third place, and ratings
+        first_place, second_place, third_place, first_rating, second_rating, third_rating = "", "", "", 0, 0, 0
+
+        # Find the highest rated recipe and set that to first
+        self.my_recipe_cards.each { |recipe_card|   
+            first_place, first_rating = recipe_card, recipe_card.rating if recipe_card.rating > first_rating               
+        }
+
+        # Find second highest rated recipe and set that to second
+        self.my_recipe_cards.each { |recipe_card|   
+            second_place, second_rating = recipe_card, recipe_card.rating if recipe_card.rating > second_rating and recipe_card != first_place          
+        }
+
+        # Find thirtd highest rated recipe and set that to third
+        self.my_recipe_cards.each { |recipe_card|   
+            third_place, third_rating = recipe_card, recipe_card.rating if recipe_card.rating > third_rating and recipe_card != first_place and recipe_card != second_place           
+        }
+
+        top_three.push(first_place, second_place, third_place)
     end
 
     def most_recent_recipes
