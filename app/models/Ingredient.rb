d6a072c97dc59f9e6063c3afc5da364fa51f3f44
Alexander Schelchere <ascellie@gmail.com>
2019-06-11 11:25:26 -0400

    fixed a lot of functions

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -13,9 +13,28 @@ class Ingredient
     def self.all
         @@all
     end
-
+    
     def self.most_common_allergen
-        Allergy.all.map { |allergy| allergy.ingredient }.mode
+        # most common
+        mca = ""
+        mca_val = 0
+
+        # hash to store allergy.ingredient and count how many times it occurs
+        allergic_to = {}
+
+        Allergy.all.map { |allergy| 
+
+           if allergic_to.has_key?(allergy.ingredient) 
+             allergic_to[allergy.ingredient] += 1
+           else
+                allergic_to[allergy.ingredient] = 1
+           end
+
+        }
+
+        allergic_to.each { |ingredient, count| mca, mca_val = ingredient, count if mca_val < count }
+
+        mca
     end
 
 end
--- app/models/Recipe.rb
+++ app/models/Recipe.rb
@@ -16,22 +16,65 @@ class Recipe
     def self.most_popular
         # should return the recipe instance with the highest number 
         # of users (the recipe that has the most recipe cards)
-        RecipeCard.all.map { |recipe_card| recipe_card.recipe }.mode
+
+            # most popular
+            mp = ""
+            mp_val = 0
+            
+            # hash to store RecipeCard.Recipe and count how many times it occurs
+           so_popular = {}
+    
+            RecipeCard.all.map { |recipe_card| 
+    
+                if allergic_to.has_key?(recipe_card.recipe) 
+                allergic_to[recipe_card.recipe] += 1
+                else
+                allergic_to[recipe_card.recipe] = 1
+                end
+    
+            }
+    
+            so_popular.each { |recipe, count| mp, mp_val = recipe, count if mp_val < count }
+    
+            mp
+    end
+
+    # Helper method to get all my recipe cards
+    def recipe_cards_that_use_me
+        RecipeCard.all.select { |recipe_card| recipe_card.recipe == self }
     end
 
     def users
         # should return the user instances who have recipe cards with this recipe
-        RecipeCard.all.select { |recipe_card| recipe_card.user if recipe_card.recipe == self }
+        recipe_cards_that_use_me.map{ |recipe_card| recipe_card.user }
+    end
+
+    # Helper method to get all recipe ingredients for recipe
+    def my_recipe_ingredients
+        RecipeIngredient.all.select { |recipe_ingredient| recipe_ingredient.recipe == self }
     end
 
     def ingredients
         # should return all of the ingredients in this recipe
-        RecipeIngredient.all.select { |recipe_ingredient| recipe_ingredient.ingredient if recipe_ingredient.recipe == self }
+        my_recipe_ingredients.map { |recipe_ingredient| recipe_ingredient.ingredient }
     end
+
 
     def allergens
         # should return all of the Ingredients in this recipe that are allergens for Users in our system.
-        Allergy.all.select { |allergy| allergy.ingredient if allergy.recipe == self }
+
+        # allergen array to push to
+        allergen_arr = []
+
+        # for each allergy in this recipe
+        Allergy.all.each { |allergy|
+
+            # see if one of our ingredients is a match with the allergy ingredient
+            self.ingredients.each { |ingredient| allergen_arr << ingredient if allergy.ingredient == ingredient } # If it's a match, push into allergen array
+        }
+        
+        # Return allergen array
+        allergen_arr.uniq!
     end
 
     def add_ingredients(ingredients_array)
